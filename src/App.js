51b514d6c37b1551d85fbc63f08481bf50729aac
AzureAD\KennethJohnMatos <kmatos@yotpo.com>
2025-03-19 15:43:51 +0800

    Yamazaki Europe GmbH Widgets 3.0

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "1056472";
+  let InstanceId_Widget = "598595";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
-  let InstanceId_Carousel = "1057226";
-  let InstanceId_StarRating = "1057216";
+  let InstanceId_Carousel = "";
+  let InstanceId_StarRating = "";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "2003273";
+  let ProductId = "4406185951287";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/7jIFwmqen0I2KGPYiLAEtg24WilUNHy9QOaEIpSC" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/6150wESsVae4cipCoJeerYEJLLT1gejxSmsCenEN" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
