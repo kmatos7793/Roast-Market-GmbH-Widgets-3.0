fb68cbc7de7b73525d40b58554f718d6411c6e7d
Kenneth John Matos <kmatos@yotpo.com>
2025-04-17 01:52:17 +0800

    Roast Market GmbH Widgets 3.0

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "1079619";
+  let InstanceId_Widget = "856616";
   let InstanceId_QA = "";
-  let InstanceId_SEO = "1079624";
+  let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "1079622";
+  let InstanceId_StarRating = "877314";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "4555881676940";
+  let ProductId = "9235";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
